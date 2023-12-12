89ce1e13291cd7b2ada3a99ac484278a73e68700
Konstantin Lübeck <konze@Konstantins-MBP.fritz.box>
2023-12-12 05:30:06 +0100

    Fixed nvim Telescope.

--- .config/nvim/lua/konze/plugins-setup.lua
+++ .config/nvim/lua/konze/plugins-setup.lua
@@ -58,7 +58,7 @@ return packer.startup(function(use)
 
     -- fuzzy finding w/ telescope
     --use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
-    use({ "nvim-telescope/telescope.nvim", tag = "0.1.1" }) -- fuzzy finder
+    use({ "nvim-telescope/telescope.nvim", tag = "0.1.4" }) -- fuzzy finder
 
     -- auto completion
     use("hrsh7th/nvim-cmp")
