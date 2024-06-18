c024d0d4fbdf9f1162e2895f209752cfe35b4ad3
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2024-06-18 20:36:45 +0200

    Tried to fix bufferline color. Not working :(

--- .config/nvim/lua/konze/core/colorscheme.lua
+++ .config/nvim/lua/konze/core/colorscheme.lua
@@ -1,5 +1,3 @@
-vim.opt.termguicolors = true
-
 require('neosolarized').setup({
     comment_italics = true,
     background_set = true,
--- .config/nvim/lua/konze/plugins/bufferline.lua
+++ .config/nvim/lua/konze/plugins/bufferline.lua
@@ -1,3 +1,5 @@
+vim.opt.termguicolors = true
+
 local status, bufferline = pcall(require, "bufferline")
 if not status then
     return
@@ -7,7 +9,7 @@ bufferline.setup({
     options = {
         mode = "buffers",
         close_command = "Bdelete! %d", 
-        -- separator_style = "slant",
+        separator_style = "slant",
         -- separator_style customization: https://github.com/akinsho/bufferline.nvim/issues/555#issuecomment-1250093105
         offsets = {
             {
