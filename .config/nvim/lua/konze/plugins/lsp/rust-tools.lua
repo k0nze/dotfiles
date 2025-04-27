2aef859a93949a2f7bf88806657e732b591fead2
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2025-04-27 08:26:13 +0200

    Trying to make rust work again.

--- .config/nvim/lua/konze/plugins/lsp/rust-tools.lua
+++ .config/nvim/lua/konze/plugins/lsp/rust-tools.lua
@@ -4,13 +4,13 @@ if not status then
 end
 
 rust_tools.setup({
-    server = {
-        on_attach = function(_, bufnr)
-            -- hover actions
-            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-            -- code action groups
-            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-        end,
-    },
+    --server = {
+    --    on_attach = function(_, bufnr)
+    --        -- hover actions
+    --        vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+    --        -- code action groups
+    --        vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    --    end,
+    --},
 })
 
