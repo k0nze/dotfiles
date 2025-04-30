a4c7b3ae357471406b111ff4462670e630f197a9
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2025-04-30 08:56:30 +0200

    Added Language tool integration.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,18 +1,17 @@
 -- adapted from: https://github.com/jakobwesthoff/nvim-from-scratch/blob/session/04/lua/plugins/lsp.lua
-
 return {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
     dependencies = {
-      -- Automatically install LSPs and related tools to stdpath for Neovim
-      -- Mason must be loaded before its dependents so we need to set it up here.
-      -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
-      { 'williamboman/mason.nvim', opts = {} },
-      'williamboman/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
+        -- Automatically install LSPs and related tools to stdpath for Neovim
+        -- Mason must be loaded before its dependents so we need to set it up here.
+        -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
+        { 'williamboman/mason.nvim', opts = {} },
+        'williamboman/mason-lspconfig.nvim',
+        'WhoIsSethDaniel/mason-tool-installer.nvim',
 
-      -- Useful status updates for LSP.
-      { 'j-hui/fidget.nvim', opts = {} },
+        -- Useful status updates for LSP.
+        { 'j-hui/fidget.nvim', opts = {} },
     },
     config = function()
       -- Brief aside: **What is LSP?**
@@ -184,6 +183,42 @@ return {
       local original_capabilities = vim.lsp.protocol.make_client_capabilities()
       local capabilities = require("blink.cmp").get_lsp_capabilities(original_capabilities)
 
+      -- adapted from: https://stackoverflow.com/a/4991602
+      function file_exists(path)
+        local f = io.open(path, "r")
+        if f~=nil then io.close(f) return true else return false end
+      end
+      
+      local ltex_opts = { 
+        filetypes = { "latex", "tex", "bib", "markdown", "gitcommit", "text" }, 
+            settings = {
+                ltex = {
+                    enabled = { "latex", "tex", "bib", "markdown", },
+                    language = "en-US",
+                    diagnosticSeverity = "information",
+                    sentenceCacheSize = 2000,
+                    languageToolHttpServerUri = "https://api.languagetoolplus.com/v2",
+                    -- languageToolOrg = {
+                    --     username = LanguageTool_username,  
+                    --     apiKey = LanguageTool_ApiKey,
+                    -- },
+                },
+            },
+        }
+
+      t = {}
+
+      local current_dir_path = string.sub(debug.getinfo(1).source, 2, string.len('/lsp.lua') * -1)
+      local language_tool_credentials_path = current_dir_path .. "../config/language-tool-credentials.lua"
+
+      if file_exists(language_tool_credentials_path) then
+        require("config.language-tool-credentials")
+        ltex_opts["settings"]["ltex"]["languageToolOrg"] = {
+            username = LanguageTool_username,  
+            apiKey = LanguageTool_ApiKey,
+        }
+      end
+
       -- capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
       -- Enable the following language servers
@@ -199,18 +234,8 @@ return {
         bashls = {},
         marksman = {},
         clangd = {},
-        -- gopls = {},
         pyright = {},
-        --rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-
+        ltex = ltex_opts, 
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
