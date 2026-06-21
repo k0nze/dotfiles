ce03f27b7702796d40036b7a9b90cd021a6c09c2
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2026-06-21 07:55:06 +0200

    Neotree updates when files on disk change.

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -11,6 +11,9 @@ return {
     ---@module "neo-tree"
     ---@type neotree.Config?
     opts = {
+        filesystem = {
+            use_libuv_file_watcher = true,
+        },
         window = {
             width = 28
         }
